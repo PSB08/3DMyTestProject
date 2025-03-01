a94d7aefebb824f8bbf7b541566d9d2997a34c01
PSB08 <Parkseongbin08@gmail.com>
2025-03-01 15:48:23 +0900

    TurnManager annotation delete

--- Assets/A.Work/01.Scripts/02.TurnGame/Manager/TurnManager.cs
+++ Assets/A.Work/01.Scripts/02.TurnGame/Manager/TurnManager.cs
@@ -6,27 +6,25 @@ using UnityEngine;
 
 public class TurnManager : MonoBehaviour
 {
-    public int currentTurnIndex = 0; // 현재 턴 인덱스
-    public List<object> turnOrder = new List<object>(); // Player와 Enemy를 모두 포함할 수 있는 리스트
+    public int currentTurnIndex = 0;
+    public List<object> turnOrder = new List<object>();
 
     public void SetupTurnOrder(List<Player> playerTeam, List<Enemy> enemyTeam)
     {
         turnOrder.AddRange(playerTeam);
         turnOrder.AddRange(enemyTeam);
-        turnOrder = turnOrder.OrderByDescending(c => (c is Player player) ? player.Speed : (c as Enemy).Speed).ToList(); // 속도에 따라 정렬
+        turnOrder = turnOrder.OrderByDescending(c => (c is Player player) ? player.Speed : (c as Enemy).Speed).ToList();
     }
 
     public void NextTurn()
     {
         currentTurnIndex++;
         if (currentTurnIndex >= turnOrder.Count)
-            currentTurnIndex = 0; // 인덱스를 0으로 초기화
+            currentTurnIndex = 0;
 
-        // 현재 턴의 캐릭터에게 행동할 기회를 줌
         object currentCharacter = turnOrder[currentTurnIndex];
         Debug.Log($"{(currentCharacter is Player ? (currentCharacter as Player).CharacterName : (currentCharacter as Enemy).CharacterName)}의 턴입니다.");
 
-        // 적의 턴일 경우 자동으로 공격
         if (currentCharacter is Enemy enemy)
         {
             enemy.AttackRandomPlayer();
@@ -37,7 +35,7 @@ public class TurnManager : MonoBehaviour
     private IEnumerator WaitTime()
     {
         yield return new WaitForSeconds(1.5f);
-        NextTurn(); // 적 공격 후 다음 턴으로 넘어가기
+        NextTurn();
         yield return new WaitForSeconds(1.5f);
     }
 
