898e7978ebedb391bf3d4d8377ff05af6e3b77fd
PSB08 <Parkseongbin08@gmail.com>
2025-03-09 23:03:50 +0900

    Player2

--- Assets/A.Work/01.Scripts/05.TopDownView/PlayerChange.cs
+++ Assets/A.Work/01.Scripts/05.TopDownView/PlayerChange.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -7,10 +8,35 @@ namespace TopDownView.Player
     {
         [SerializeField] private GameObject player1;
         [SerializeField] private GameObject player2;
-        [SerializeField] private CinemachineCamera cam1;
-        [SerializeField] private CinemachineCamera cam2;
+        [SerializeField] private TextMeshProUGUI text1;
+        [SerializeField] private TextMeshProUGUI text2;
+
+        private void Start()
+        {
+            player1.gameObject.SetActive(true);
+            player2.gameObject.SetActive(false);
+            text1.gameObject.SetActive(true);
+            text2.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                player1.gameObject.SetActive(true);
+                player2.gameObject.SetActive(false);
+                text1.gameObject.SetActive(true);
+                text2.gameObject.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                player1.gameObject.SetActive(false);
+                player2.gameObject.SetActive(true);
+                text1.gameObject.SetActive(false);
+                text2.gameObject.SetActive(true);
+            }
+
+        }
 
     }
-
-
 }
--- Assets/A.Work/01.Scripts/05.TopDownView/SecondPlayer/Player2.cs
+++ Assets/A.Work/01.Scripts/05.TopDownView/SecondPlayer/Player2.cs
@@ -1,10 +1,60 @@
+using TMPro;
+using TopDownView.Player;
 using UnityEngine;
 
 namespace TopDownView.SecondPlayer
 {
     public class Player2 : MonoBehaviour
     {
+        [SerializeField] private CharacterMovement2 movement;
+        [SerializeField] private PlayerInputSO2 playerInput;
+        [SerializeField] private TextMeshProUGUI sprintTxt;
+
+        private void Start()
+        {
+            sprintTxt.text = "Walk";
+        }
+
+        private void Awake()
+        {
+            playerInput.OnMovementChange += HandleMovementChange;
+            playerInput.OnJumpPressed += HandleJumpPressed;
+            playerInput.OnRunPressed += HandleRunPressed;
+            playerInput.OnAttackPressed += HandleAttackPressed;
+        }
+
+        private void OnDestroy()
+        {
+            playerInput.OnMovementChange -= HandleMovementChange;
+            playerInput.OnJumpPressed -= HandleJumpPressed;
+            playerInput.OnRunPressed -= HandleRunPressed;
+            playerInput.OnAttackPressed -= HandleAttackPressed;
+        }
+
+        private void HandleMovementChange(Vector2 movementnput)
+        {
+            movement.SetMovementDirection(movementnput);
+        }
+
+        private void HandleJumpPressed()
+        {
+            movement.Jump();
+        }
+
+        public void HandleRunPressed(bool isSprinting)
+        {
+            this.movement.SetSprint(isSprinting);
+            if (isSprinting)
+                sprintTxt.text = "Running";
+            else
+                sprintTxt.text = "Walk";
+        }
+
+        private void HandleAttackPressed()
+        {
+
+        }
+
 
     }
-
 }
