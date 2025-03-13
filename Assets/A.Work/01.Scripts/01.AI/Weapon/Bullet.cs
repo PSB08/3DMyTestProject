97e12111e68512f609c14f237ed211816f4c5a63
PSB08 <Parkseongbin08@gmail.com>
2025-03-14 08:48:42 +0900

    utf - 8 -> x-windows-949

--- Assets/A.Work/01.Scripts/01.AI/Weapon/Bullet.cs
+++ Assets/A.Work/01.Scripts/01.AI/Weapon/Bullet.cs
@@ -10,7 +10,7 @@ public class Bullet : MonoBehaviour
     private void Start()
     {
         GetComponent<Rigidbody>().linearVelocity = transform.forward * speed;
-        Destroy(gameObject, lifeTime); // ���� �ð� �� �Ѿ� ����
+        Destroy(gameObject, lifeTime);
     }
 
     
