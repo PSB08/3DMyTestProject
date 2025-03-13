ab90e7226dd9cb0b8cb1a951550b6bd7e17797ee
PSB08 <Parkseongbin08@gmail.com>
2025-03-13 19:56:32 +0900

    Code delete

--- Assets/A.Work/01.Scripts/06.Reflection/MainReflection.cs
+++ Assets/A.Work/01.Scripts/06.Reflection/MainReflection.cs
@@ -6,39 +6,6 @@ namespace GetType
 {
     public class MainReflection : MonoBehaviour
     {
-        static void PrintInterfaces(Type type)
-        {
-            Console.WriteLine("-----Interfaces-----");
-
-            Type[] interfaces = type.GetInterfaces();
-
-            foreach (Type i in interfaces)
-            {
-                Console.WriteLine($"Name : {0}", i.Name);
-            }
-            Console.WriteLine();
-        }
-
-        static void PrintFields(Type type)
-        {
-            Console.WriteLine("-----Fields-----");
-
-            FieldInfo[] fields = type.GetFields
-                (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
-
-            foreach (FieldInfo field in fields)
-            {
-                String accessLevel = "protected";
-
-                if (field.IsPublic) accessLevel = "public";
-                else if (field.IsPrivate) accessLevel = "private";
-
-                Console.WriteLine($"Access : {0}, Type : {1}, Name : {2}", accessLevel, field.FieldType.Name, field.Name);
-
-            }
-            Console.WriteLine();
-        }
-
 
 
     }
