00bd1e1fcacbc79240016c9e1f79039be37e79f3
PSB08 <Parkseongbin08@gmail.com>
2025-03-16 22:19:12 +0900

    CreateInstance code

--- Assets/A.Work/01.Scripts/06.Reflection/CreateInstanceTest.cs
+++ Assets/A.Work/01.Scripts/06.Reflection/CreateInstanceTest.cs
@@ -1,10 +1,31 @@
+using System;
+using System.Reflection;
 using UnityEngine;
 
 namespace ReflectTest.CreateInstance
 {
-    public class CreateInstanceTest : MonoBehaviour
+    class Profile
     {
-
+        public string Name { get; set; }   
+        public string PhoneNum { get; set; }   
     }
 
+    public class CreateInstanceTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            Type type = typeof(Profile);
+            object profile = Activator.CreateInstance(type);
+
+            PropertyInfo name = type.GetProperty("Name");
+            PropertyInfo phoneNum = type.GetProperty("PhoneNum");
+
+            name.SetValue(profile, "ȫ�浿", null);
+            phoneNum.SetValue(profile, "010-1234-1234", null);
+
+            Debug.Log($"{name.GetValue(profile, null)}, {phoneNum.GetValue(profile, null)}");
+        }
+
+
+    }
 }
