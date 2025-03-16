426e97ec53a3d5b908aff25aa6bde2523952841f
PSB08 <Parkseongbin08@gmail.com>
2025-03-16 14:22:09 +0900

    MainReflection code

--- Assets/A.Work/01.Scripts/06.Reflection/MainReflection.cs
+++ Assets/A.Work/01.Scripts/06.Reflection/MainReflection.cs
@@ -6,7 +6,51 @@ namespace GetType
 {
     public class MainReflection : MonoBehaviour
     {
-        
+        private void Start()
+        {
+            int a = 0;
+            Type type = a.GetType();
+
+            PrintInterfaces(type);
+            PrintFields(type);
+
+            Type b = typeof(int);
+            Console.WriteLine(b.Name);
+        }
+
+        private static void PrintInterfaces(Type type)
+        {
+            Debug.Log("-----Interfaces-----");
+
+            Type[] interfaces = type.GetInterfaces();
+
+            foreach (Type i in interfaces)
+            {
+                Debug.Log($"Name : {i.Name}");
+            }
+            Debug.Log("");
+        }
+
+        static void PrintFields(Type type)
+        {
+            Debug.Log("-----Fields-----");
+
+            FieldInfo[] fields = type.GetFields
+                (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields)
+            {
+                String accessLevel = "protected";
+
+                if (field.IsPublic) accessLevel = "public";
+                else if (field.IsPrivate) accessLevel = "private";
+
+                Debug.Log($"Access : {accessLevel}, Type : {field.FieldType.Name}, Name : {field.Name}");
+
+            }
+            Debug.Log("");
+        }
+
 
     }
 }
