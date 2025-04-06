e67b95a613147add9a58b139fad68fb1f0e68696
PSB08 <Parkseongbin08@gmail.com>
2025-04-06 20:49:41 +0900

    feat : 09.EngineNavTest

---
+++ Assets/00.Work/01.Scripts/Code/FSM/EntityState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Code.Entities;
+
+public abstract class EntityState
+{
+
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Update(){ }
+
+    public virtual void Exit()
+    {
+    }
+    
+    
+    
+}
--- Assets/A.Work/01.Scripts/05.TopDownView/TestPJ/Blade/Player/States/EntityState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewEntityState", menuName = "EntityState")]
-public class EntityState : ScriptableObject
-{
-    public virtual void Enter(PlayerStateMachine player) {}
-    public virtual void Exit(PlayerStateMachine player) {}
-    public virtual void Handle(PlayerStateMachine player) {} 
-}
