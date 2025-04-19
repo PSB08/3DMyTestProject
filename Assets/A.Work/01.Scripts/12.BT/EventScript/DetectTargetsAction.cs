009069dab2fc0e13c23c1b9fb12b1b826bbbea85
PSB08 <Parkseongbin08@gmail.com>
2025-04-20 02:45:55 +0900

    feat : BT finish, sensor script

--- Assets/A.Work/01.Scripts/12.BT/EventScript/DetectTargetsAction.cs
+++ Assets/A.Work/01.Scripts/12.BT/EventScript/DetectTargetsAction.cs
@@ -3,6 +3,7 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using UnityEngine.AI;
 
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "DetectTargets", story: "[Agent] detects [Target]", category: "Action", id: "59cd33ca7db33693cd54b29bf3eaed6b")]
@@ -11,13 +12,22 @@ public partial class DetectTargetsAction : Action
     [SerializeReference] public BlackboardVariable<GameObject> Agent;
     [SerializeReference] public BlackboardVariable<GameObject> Target;
 
+    private NavMeshAgent agent;
+    private Sensor sensor;
+    
     protected override Status OnStart()
     {
+        agent = Agent.Value.GetComponent<NavMeshAgent>();
+        sensor = Agent.Value.GetComponent<Sensor>();
         return Status.Running;
     }
 
     protected override Status OnUpdate()
     {
+        var target = sensor.GetClosestTarget("Targets");
+        if (target == null) return Status.Running;
+        
+        Target.Value = target.gameObject;
         return Status.Success;
     }
 
