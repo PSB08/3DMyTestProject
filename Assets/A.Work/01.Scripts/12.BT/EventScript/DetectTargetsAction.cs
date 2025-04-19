6ec7e72d06ecca8dc8226f85201c9fc94ad5a10f
PSB08 <Parkseongbin08@gmail.com>
2025-04-20 02:10:09 +0900

    feat : BT graph, cs

---
+++ Assets/A.Work/01.Scripts/12.BT/EventScript/DetectTargetsAction.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "DetectTargets", story: "[Agent] detects [Target]", category: "Action", id: "59cd33ca7db33693cd54b29bf3eaed6b")]
+public partial class DetectTargetsAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
