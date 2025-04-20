0c1b0f2e70ab881a0cb3f460ef3c1e907716b191
PSB08 <Parkseongbin08@gmail.com>
2025-04-20 22:30:13 +0900

    feat : EnemyBT

---
+++ Assets/A.Work/02.Assets/12.BTEnemyFolder/_Scripts/Events/RangeDetectorAction.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Range Detector", story: "Update Range [Detector] and assign [Target]", category: "Action", id: "146717ce72f1a15f6aa3ebc125ee78f4")]
+public partial class RangeDetectorAction : Action
+{
+    [SerializeReference] public BlackboardVariable<RangeDetector> Detector;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
