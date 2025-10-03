ae56e53c044a973faa42059296e8822ab0a9eea0
PSB08 <Parkseongbin08@gmail.com>
2025-10-03 21:17:11 +0900

    feat : Asset

---
+++ Assets/A.Work/01.Scripts/15.EditorClass/Editor/EGWKnob.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Scripts.EditorClass
+{
+    public class EGWKnob : EditorWindow
+    {
+        float angle = 0;
+
+        [MenuItem("Window/EGW/EGWKnob")]
+        static void Open()
+        {
+            GetWindow<EGWKnob>();
+        }
+
+        void OnGUI()
+        {
+            angle = EditorGUILayout.Knob(Vector2.one * 64, angle, 0, 360, "도", Color.gray, Color.red, true);
+        }
+    }
+}
---
+++ Assets/A.Work/01.Scripts/15.EditorClass/Editor/EGWScope.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Scripts.EditorClass
+{
+    public class EGWScope : EditorWindow
+    {
+        [MenuItem("Window/EGW/EGWScope")]
+        static void Open()
+        {
+            GetWindow<EGWScope>();
+        }
+
+        void OnGUI()
+        {
+            using (new BackgroundColorScope(Color.green))
+            {
+                GUILayout.Button("Button1");
+                using (new BackgroundColorScope(Color.yellow))
+                {
+                    GUILayout.Button("Button2");
+                }
+            }
+        }
+    }
+}
+
+public class HorizontalScope : GUI.Scope
+{
+    public HorizontalScope()
+    {
+        EditorGUILayout.BeginHorizontal();
+    }
+    
+    protected override void CloseScope()
+    {
+        EditorGUILayout.EndHorizontal();   
+    }
+}
+
+public class BackgroundColorScope : GUI.Scope
+{
+    private readonly Color _originalColor;
+
+    public BackgroundColorScope(Color newColor)
+    {
+        _originalColor = GUI.backgroundColor;
+        GUI.backgroundColor = newColor;
+    }
+
+    protected override void CloseScope()
+    {
+        GUI.backgroundColor = _originalColor;
+    }
+}
